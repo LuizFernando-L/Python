67660b16d291891f2608f535eabb4dc6c0fa4fb5
Luiz Fernando <luizfernando.dev06@gmail.com>
2022-10-24 20:05:33 -0300

    commit

---
+++ EX003/SomandoDoisNumeros.py
@@ -0,0 +1,5 @@
+#Vamos primeiro pedir ao usuário digitar dois números
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite um segundo número: "))
+soma = num1 + num2
+print(f"O resultado da soma entre {num1} e {num2} é igual a {soma}")
