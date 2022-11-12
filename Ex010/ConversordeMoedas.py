80bcf4a83bde6d77df021030ce19608b07035dbc
Luiz Fernando <luizfernando.dev06@gmail.com>
2022-11-12 15:53:55 -0300

    commit

--- Ex010/ConversordeMoedas.py
+++ Ex010/ConversordeMoedas.py
@@ -1,4 +1,4 @@
-dinheiro = float(input("Quanto de dinheiro você tem na carteira? "))
-dolar = 5.02
-qtd = dinheiro / dolar 
-print(f"Você pode comprar $ {dolar * qtd }")
+dinheiro = float(input("Quanto de dinheiro você tem na carteira? R$ "))
+qtd = dinheiro/5.02
+
+print(f"Com R$ {dinheiro} você pode comprar $1{qtd} ")
