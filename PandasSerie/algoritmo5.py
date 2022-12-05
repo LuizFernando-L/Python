0abb57b8477bedc0b832efa030bf7129f037445c
Luiz Fernando <luizfernando.dev06@gmail.com>
2022-12-05 11:00:44 -0300

    commit

--- PandasSerie/algoritmo5.py
+++ PandasSerie/algoritmo5.py
@@ -67,4 +67,25 @@ pd.DataFrame(lista_nomes, columns=['nome'])
 
 pd.DataFrame(lista_nomes, columns=['nome'], index=lista_cpfs)
 
+#in 11
+
+dados = list(zip(lista_nomes, lista_cpfs, lista_idades, lista_emails)) # cria uma lista de tuplas
+
+pd.DataFrame(dados, columns=['nome', 'cpfs', 'idade', 'email'])
+
+#in 12
+dados = {
+
+    'nomes': 'Howard Ian Peter Jonah Kellie'.split(),
+
+    'cpfs' : '111.111.111-11 222.222.222-22 333.333.333-33 444.444.444-44 555.555.555-55'.split(),
+
+    'emails' : 'risus.varius@dictumPhasellusin.ca Nunc@vulputate.ca fames.ac.turpis@cursusa.org non@felisullamcorper.org eget.dictum.placerat@necluctus.co.uk'.split(),
+
+    'idades' : [32, 22, 25, 29, 38]
+
+}
+
+pd.DataFrame(dados)
+
 
